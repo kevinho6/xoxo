2c872db09f51f236ce4537b6d188eb980fc5864c
Kevin Ho <bornarkun@gmail.com>
2018-07-05 14:25:17 -0400

    bugs

--- game/index.js
+++ game/index.js
@@ -1,6 +1,26 @@
-import {Map} from 'immutable'
+/* eslint-disable */
+import { Map } from 'immutable';
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+let board = Map();
+
+const move = (turn, position) => ({
+  type: 'MOVE',
+  position: position,
+  player: turn,
+});
+
+export default function reducer(state = { game: Map() }, action) {
+  switch (action.type) {
+    case 'MOVE':
+      if (state.player === 'X') {
+        let nextPlayer = 'O';
+      } else {
+        let nextPlayer = 'X';
+      }
+      board = state.game.setIn(state.position, state.player);
+      return { ...state, game: board, player: nextPlayer };
+
+    default:
+      return state;
+  }
 }
